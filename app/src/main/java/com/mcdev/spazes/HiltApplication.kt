9dc2a3d02c98c114ad76b192105cd07e8e89594a
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2022-06-25 13:36:30 +0000

    Add custom crash activity

--- app/src/main/java/com/mcdev/spazes/HiltApplication.kt
+++ app/src/main/java/com/mcdev/spazes/HiltApplication.kt
@@ -1,8 +1,27 @@
 package com.mcdev.spazes
 
 import android.app.Application
+import cat.ereza.customactivityoncrash.config.CaocConfig
+import com.mcdev.spazes.ui.HomeActivity
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HiltApplication : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        CaocConfig.Builder.create()
+            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
+            .enabled(true) //default: true
+            .showErrorDetails(false) //default: true
+            .showRestartButton(true) //default: true
+            .logErrorOnRestart(false) //default: true
+            .trackActivities(false) //default: false
+            .minTimeBetweenCrashesMs(2000) //default: 3000
+//            .errorDrawable(R.drawable.ic_custom_drawable) //default: bug image
+            .restartActivity(HomeActivity::class.java) //default: null (your app's launch activity)
+//                .errorActivity(YourCustomErrorActivity.class) //default: null (default error activity)
+//                    .eventListener(new YourCustomEventListener()) //default: null
+//                    .customCrashDataCollector(new YourCustomCrashDataCollector()) //default: null
+            .apply()
+    }
 }
